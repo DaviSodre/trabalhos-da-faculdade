df1bc553e8baa7126ab4bb7a69257032e068a51d
Davi Sodré <124099906+DaviSodre@users.noreply.github.com>
2025-04-14 19:47:19 -0300

    Create SistemaAntigo.java

---
+++ Trabalhos Java/SistemaPagamento/SistemaAntigo.java
@@ -0,0 +1,8 @@
+package SistemaPagamento;
+
+public class SistemaAntigo {
+	public void realizarPagamento(double valor) {
+		System.out.println("Pagamento realizado no sistema antigo: R$" + valor);
+	}
+
+}
