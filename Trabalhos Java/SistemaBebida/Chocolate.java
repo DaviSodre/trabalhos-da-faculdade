be54a395197e5192b2cf00e13605897191dacde2
Davi Sodré <124099906+DaviSodre@users.noreply.github.com>
2025-04-14 20:34:27 -0300

    Update Chocolate.java

--- Trabalhos Java/SistemaBebida/Chocolate.java
+++ Trabalhos Java/SistemaBebida/Chocolate.java
@@ -1,1 +1,23 @@
 
+package ProjetoBebida;
+
+public class Chocolate extends AdicionalDecorator {
+	
+		public Chocolate(Bebida bebida) {
+			super(bebida);
+		}
+		
+		@Override
+		public String getDescricao() {
+			return bebida.getDescricao() + " com Chocolate";
+		}
+		
+		@Override
+		public double getPreco() {
+			return bebida.getPreco() + 2.0;
+		}
+
+	}
+
+
+
