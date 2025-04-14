fb321771232412fa74b08383e149b09c77e60e5f
Davi Sodré <124099906+DaviSodre@users.noreply.github.com>
2025-04-14 19:42:45 -0300

    Delete trabalho java 01/SistemaAntigo.java

--- trabalho java 01/SistemaAntigo.java
+++
@@ -1,8 +0,0 @@
-package SistemaPagamento;
-
-public class SistemaAntigo {
-	public void realizarPagamento(double valor) {
-		System.out.println("Pagamento realizado no sistema antigo: R$" + valor);
-	}
-
-}
